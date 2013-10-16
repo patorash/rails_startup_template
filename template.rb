d73bd25df704557fc6ff30cb3e1d6e9e62dd2529
patorash <chariderpato@gmail.com>
2013-10-17 01:58:15 +0900

    俺のtemplate.rbを作成中

--- template.rb
+++ template.rb
@@ -10,29 +10,65 @@ gem "bourbon"
 
 # For authorization (https://github.com/ryanb/cancan)
 gem "cancan"
+if yes?('Use devise?')
+  gem 'devise'
+  use_devise = true
+end
+
+gem 'kaminari'
+gem 'ransack'
+gem 'ancestry'
+gem 'active_decorator'
+gem 'whenever', require: false
+
+if yes?("Use Bootstrap?")
+  gem 'therubyracer', platforms: :ruby
+  gem 'less-rails'
+  gem 'twitter-bootstrap-rails'
+  use_bootstrap = true
+end
 
 # HAML templating language (http://haml.info)
-gem "haml-rails" if yes?("Use HAML instead of ERB?")
+#gem "haml-rails" if yes?("Use HAML instead of ERB?")
+gem 'slim-rails' if yes?("Use slim instead of ERB?")
+
 
 # Simple form builder (https://github.com/plataformatec/simple_form)
-gem "simple_form", git: "https://github.com/plataformatec/simple_form"
+#gem "simple_form", git: "https://github.com/plataformatec/simple_form"
+gem "simple_form", github: 'plataformatec/simple_form', branch: 'master'
 # To generate UUIDs, useful for various things
 gem "uuidtools"
+
+gem_group :development, :test do
+  gem "rspec-rails"
+  # Capybara for integration testing (https://github.com/jnicklas/capybara)
+  gem "capybara"
+  gem "capybara-webkit"
+  gem 'debugger'
+end
 
 gem_group :development do
   # Rspec for tests (https://github.com/rspec/rspec-rails)
   gem "rspec-rails"
   # Guard for automatically launching your specs when files are modified. (https://github.com/guard/guard-rspec)
   gem "guard-rspec"
+  gem 'better_errors'
+  gem 'spring'
+  gem 'capistrano', "~> 2.15.5"
+  gem 'capistrano_colors'
+  gem 'capistrano-ext'
+  gem 'capistrano-rbenv'
 end
 
 gem_group :test do
-  gem "rspec-rails"
-  # Capybara for integration testing (https://github.com/jnicklas/capybara)
-  gem "capybara" 
-  gem "capybara-webkit"
+  gem 'database_cleaner'
+  gem 'launchy'
+  gem 'rb-fsevent', require: false
+  gem 'growl'
   # FactoryGirl instead of Rails fixtures (https://github.com/thoughtbot/factory_girl)
   gem "factory_girl_rails"
+  gem 'simplecov', require: false
+  gem 'webmock', require: 'webmock/rspec'
 end
 
 gem_group :production do
@@ -45,24 +81,54 @@ end
 # https://github.com/ddollar/foreman
 # ==================================================
 # Use Procfile for foreman
-run "echo 'web: bundle exec rails server -p $PORT' >> Procfile"
-run "echo PORT=3000 >> .env"
-run "echo '.env' >> .gitignore"
+if yes?('Use unicorn?')
+  gem 'unicorn'
+  run "echo 'web: bundle exec unicorn -p $PORT -c ./config/unicorn.conf.rb' >> Procfile"
+else
+  run "echo 'web: bundle exec rails server -p $PORT' >> Procfile"
+end
+
+run "echo PORT=3000 >> .foreman"
+run "echo '.foreman' >> .gitignore"
 # We need this with foreman to see log output immediately
 run "echo 'STDOUT.sync = true' >> config/environments/development.rb"
 
+run "bundle install"
 
-
-# Initialize guard
+# Initialize Devise
 # ==================================================
-run "bundle exec guard init rspec"
-
-
+if use_devise
+  run "rails g devise:install"
+  run "rails g devise user"
+  run "rails g devise:views"
+  run "echo 'config.action_mailer.default_url_options = { :host => 'localhost:3000' }' >> config/environments/development.rb"
+end
 
 # Initialize CanCan
 # ==================================================
 run "rails g cancan:ability"
 
+# Initialize Bootstrap
+# ==================================================
+if use_bootstrap
+  run "rails g bootstrap:install less"
+  if yes?("Responsive layout?")
+    run "rails g bootstrap:layout application fluid"
+  else
+    run "rails g bootstrap:layout application fixed"
+  end
+  run "rails g simple_form:install --bootstrap"
+end
+
+
+
+# Initialize rspec
+# ==================================================
+run "rails g rspec:install"
+
+# Initialize guard
+# ==================================================
+run "bundle exec guard init rspec"
 
 
 # Clean up Assets
@@ -83,15 +149,15 @@ run "echo '@import \"bourbon\";' >>  app/assets/stylesheets/application.css.scss
 # Bootstrap: install from https://github.com/twbs/bootstrap
 # Note: This is 3.0.0
 # ==================================================
-if yes?("Download bootstrap?")
-  run "wget https://github.com/twbs/bootstrap/archive/v3.0.0.zip -O bootstrap.zip -O bootstrap.zip"
-  run "unzip bootstrap.zip -d bootstrap && rm bootstrap.zip"
-  run "cp bootstrap/bootstrap-3.0.0/dist/css/bootstrap.css vendor/assets/stylesheets/"
-  run "cp bootstrap/bootstrap-3.0.0/dist/js/bootstrap.js vendor/assets/javascripts/"
-  run "rm -rf bootstrap"
-  run "echo '@import \"bootstrap\";' >>  app/assets/stylesheets/application.css.scss"
-  run "rails g simple_form:install --bootstrap"
-end
+#if yes?("Download bootstrap?")
+#  run "wget https://github.com/twbs/bootstrap/archive/v3.0.0.zip -O bootstrap.zip -O bootstrap.zip"
+#  run "unzip bootstrap.zip -d bootstrap && rm bootstrap.zip"
+#  run "cp bootstrap/bootstrap-3.0.0/dist/css/bootstrap.css vendor/assets/stylesheets/"
+#  run "cp bootstrap/bootstrap-3.0.0/dist/js/bootstrap.js vendor/assets/javascripts/"
+#  run "rm -rf bootstrap"
+#  run "echo '@import \"bootstrap\";' >>  app/assets/stylesheets/application.css.scss"
+#  run "rails g simple_form:install --bootstrap"
+#end
 
 
 # Font-awesome: Install from http://fortawesome.github.io/Font-Awesome/
@@ -124,6 +190,8 @@ doc/
 .DS_Store
 EOF"
 
+
+route "root to: 'welcome#index'"
 
 # Git: Initialize
 # ==================================================
